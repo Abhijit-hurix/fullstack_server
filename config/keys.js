cec4e2ba1d61aba555e38592bf16495a821072b0
abhijit-mitra <abhijitmitra.in@gmail.com>
2017-10-29 23:34:54 +0530

    changes in keys file

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 if(process.env.NODE_ENV === 'production'){
 	//we are in prodction return the prood set of keys
+	module.exports = require('./prod');
 }else{
 	//we are in development return the dev keys
 	module.exports = require('./dev');
