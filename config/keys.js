ebc1c3922848027354ffc0d376a2e9b5dc9cc367
abhijit-mitra <abhijitmitra.in@gmail.com>
2017-10-29 20:28:33 +0530

    config commit

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports ={
+	googleClientID : "471227753263-ohjt1oop0rcidvgcs731p08148g0e64m.apps.googleusercontent.com",
+	googleClientSecret : "CX4a7PK9BOnVR9Y7sk7b5k1X",
+	mongoURI:'mongodb://abhijit:abhijit@ds053718.mlab.com:53718/emaily-dev',
+	/*mongoURI:'mongodb://localhost:27019/emaily-dev',*/
+	cookieKey:'hghgfhdgfhgghdbbsshdhjhaalkalakwkewkjf'
+}
